dd2fab834cfcbc44b0de053cded487a4770347cd
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-31 09:29:25 +0200

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const port = 4000
+const port = 2000
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world!')
+  res.status(200).send('Hello world!!!')
 })
 
 app.listen(port, () => {
